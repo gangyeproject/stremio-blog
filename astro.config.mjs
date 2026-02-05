beeb6932d93e959491214124635766887d896e9c
gangyeproject <gangyedede@gmail.com>
2026-02-05 20:52:59 +0800

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://stremioaddonmanager.org',
+  base: '/blog',
+  integrations: [sitemap()],
 });
